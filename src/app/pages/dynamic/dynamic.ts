e46f0f8f40b8e416d1ef604f53f2184ee5f54f83
Eugene Murray <eugene.murray@gmail.com>
2026-03-21 12:55:47 +0000

    add / remove dynamic components

--- src/app/pages/dynamic/dynamic.ts
+++ src/app/pages/dynamic/dynamic.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
+import { RedComponent } from '../../dynamic-components/red/red';
+import { BlueComponent } from '../../dynamic-components/blue/blue';
+import { GreenComponent } from '../../dynamic-components/green/green';
+import { YellowComponent } from '../../dynamic-components/yellow/yellow';
+
+interface ColorItem {
+  name: string;
+  component: Type<unknown>;
+}
 
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.html',
-  styleUrl: './dynamic.scss'
+  styleUrl: './dynamic.scss',
+  imports: [NgComponentOutlet]
 })
-export class DynamicComponent {}
+export class DynamicComponent {
+  available: ColorItem[] = [
+    { name: 'Red', component: RedComponent },
+    { name: 'Blue', component: BlueComponent },
+    { name: 'Green', component: GreenComponent },
+    { name: 'Yellow', component: YellowComponent },
+  ];
+
+  active: ColorItem[] = [];
+
+  add(item: ColorItem): void {
+    this.active.push(item);
+    this.available = this.available.filter(c => c !== item);
+  }
+
+  remove(item: ColorItem): void {
+    this.available.push(item);
+    this.active = this.active.filter(c => c !== item);
+  }
+}
