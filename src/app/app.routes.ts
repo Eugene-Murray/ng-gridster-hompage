5aa941543aa5c152cea794a3ea2d03da1adb75d4
Eugene Murray <eugene.murray@gmail.com>
2026-03-21 12:40:45 +0000

    add components and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { DynamicComponent } from './pages/dynamic/dynamic';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dynamic', component: DynamicComponent }
+];
